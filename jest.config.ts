722e4cac369d61c4561c36c93f875f974ed06c79
Molnár Bence <molnar.bence@outlook.com>
2022-03-10 20:56:38 +0100

    Switch to Next.js

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+    dir: './'
+});
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: [
+    "**/__tests__/*.(ts|tsx)"
+  ],
+  testPathIgnorePatterns: [
+      "./.next/",
+      "./node_modules/",      
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  globals: {
+    'ts-jest': {
+        tsconfig: 'tsconfig.jest.json'
+    }
+  }
+}
+
+export default createJestConfig(customJestConfig);
