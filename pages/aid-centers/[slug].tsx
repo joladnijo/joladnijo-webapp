0a46b816c7e6ae6c7438eaf0231e904aa2fd9bd7
Molnár Bence <molnar.bence@outlook.com>
2022-03-15 23:52:14 +0100

    Fetch some data from backend

--- pages/aid-centers/[slug].tsx
+++ pages/aid-centers/[slug].tsx
@@ -1,24 +1,52 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 import styles from '@/styles/Home.module.css';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import getConfig from 'next/config';
 
-const AidCenterInfoPage: NextPage = () => {
-  const router = useRouter();
-  const { slug } = router.query;
+interface AidCenterInfoPageProps {
+  id: string;
+  name: string;
+  slug: string;
+}
+
+interface AidCenterInfoPageParams {
+  [key: string]: string;
+  slug: string;
+}
+
+const { publicRuntimeConfig } = getConfig();
+const { backendApiBaseUrl } = publicRuntimeConfig;
+
+const AidCenterInfoPage: NextPage<AidCenterInfoPageProps> = ({ slug, name }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Gyűjtőközpont - {slug}</title>
+        <title>Gyűjtőközpont - {name}</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>Slug: {slug}</h1>
+        <h1 className={styles.title}>
+          Slug: {slug}, Name: {name}
+        </h1>
       </main>
     </div>
   );
 };
 
 export default AidCenterInfoPage;
+
+export const getServerSideProps: GetServerSideProps<AidCenterInfoPageProps, AidCenterInfoPageParams> = async (
+  context,
+) => {
+  const { slug: slugFromQuery } = context.params!;
+
+  const response = await fetch(`${backendApiBaseUrl}/aid-centers/${slugFromQuery}`);
+  const data = await response.json();
+  return {
+    props: {
+      ...data,
+    },
+  };
+};
