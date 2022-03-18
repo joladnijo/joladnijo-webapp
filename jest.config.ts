5b008f5def217fd5c4171f586332483229779f8a
Molnár Bence <xjarekx@gmail.com>
2022-03-18 21:26:10 +0100

    Copy template

--- jest.config.ts
+++ jest.config.ts
@@ -1,32 +1,27 @@
-import nextJest from 'next/jest'
+import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
-    dir: './'
+  dir: './',
 });
 
 const customJestConfig = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
-  testMatch: [
-    "**/__tests__/*.(ts|tsx)"
-  ],
-  testPathIgnorePatterns: [
-      "./.next/",
-      "./node_modules/",      
-  ],
+  testMatch: ['**/__tests__/*.(ts|tsx)'],
+  testPathIgnorePatterns: ['./.next/', './node_modules/'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   globals: {
     'ts-jest': {
-        tsconfig: 'tsconfig.jest.json'
-    }
-  }
-}
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
+};
 
 export default createJestConfig(customJestConfig);
---
+++ models/AidCenter.ts
@@ -0,0 +1,17 @@
+export interface AidCenter {
+  id: string;
+  note: string;
+  name: string;
+  slug: string;
+  photo: string;
+  countryCode: string;
+  postalCode: string;
+  city: string;
+  address: string;
+  geoLocation: any;
+  callRequired: string;
+  moneyAccepted: number;
+  moneyDescription: string;
+  campaignEndingOn: string;
+  organizationId: string;
+}
