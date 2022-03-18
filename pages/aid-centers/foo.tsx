2891e2a448e05ae445cc36cd4ef3b27d7057cf15
Krishtof <kristof.vizy@gmail.com>
2022-03-18 12:49:05 +0100

    aid-center sitebuild

--- pages/aid-centers/foo.tsx
+++ pages/aid-centers/foo.tsx
@@ -10,10 +10,37 @@ const FooAidCenterInfoPage: NextPage = () => {
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className=' header container mx-auto bg-accent-dark bg-cover bg-hero h-80 max-w-5xl flex items-end relative'>
+        <div className='w-full h-full absolute top-0 left-0 right-0 bottom-0 from-black bg-gradient-to-t opacity-80'></div>
+        <div className='mx-auto bg-accent-dark bg-cover bg-hero h-80 w-full h-full flex'>
+          <div className='title flex sm:items-end flex-col sm:flex-row justify-between absolute px-6 py-5	 bottom-0 left-0 right-0'>
+            <div className='left flex flex-col'>
+              <h1 className='text-white	'>Piripócs művelődési ház</h1>
+              <p className='text-white opacity-70	'>Piripócsi önkormányzat</p>
+            </div>
+            <p className='text-white opacity-60 text-sm text-right'>Utolára frissítve: 2 napja</p>
 
-      <main>
-        <h1 className={styles.title}>Ide lehet dolgozni</h1>
-      </main>
+          </div>
+        </div>
+      </div>
+
+      <div className='details container max-w-5xl min-h-screen bg-white mx-auto px-6	py-10'>
+        <div className="grid grid-cols-3 gap-4">
+            <div className='md:col-span-2'>
+              <div className='need'>
+                <h2>Adományok, amiket várunk</h2>
+                <div className='item-list flex flex-col'>
+                  <div className='item'>
+                    <div className='icon'>
+                      <i className="absolute fa fa-search text-gray-400 top-5 left-4"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('../public/images/gyujtohely-img.png')",
+      }
+    },
+
   },
   plugins: [],
 };
