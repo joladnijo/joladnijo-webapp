23b1a07fa2a299f8a2be235e446649dff23098ba
Molnár Bence <xjarekx@gmail.com>
2022-03-21 00:43:14 +0100

    Rename generate script

--- generate-backend-sdk.sh
+++ generate-backend-sdk.sh
@@ -1,12 +1,14 @@
-rm -rf backend-sdk
-mkdir backend-sdk
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
+TARGET="backend-sdk"
+
+rm -rf $TARGET
+mkdir $TARGET
+docker run --rm -v ${PWD}:/local -u $(id -u ${USER}):$(id -g ${USER}) \
+  openapitools/openapi-generator-cli generate \
  -i https://raw.githubusercontent.com/joladnijo/joladnijo-backend/31-generate-open-api-json-output/openapi-schema.json \
  -g typescript-fetch \
  --additional-properties=supportsES6=true,npmVersion=8.5.0,typescriptThreePlus=true,withoutRuntimeChecks=true,useSingleRequestParameter=false \
- -o /local/backend-sdk
+ -o /local/$TARGET
 
-TARGET="backend-sdk"
 
 SED_COMMAND='1s;^;// @ts-nocheck\n;'
 if [ "$(uname)" != "Darwin" ]; then
