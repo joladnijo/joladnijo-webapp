a7daea38f5357d65c48fc624c9dd5858f5303108
Csaba Gajdos <info@cleancut.hu>
2022-03-24 06:55:28 +0100

    visszaállítom a saját baseURL-re, hogyha kell, akkor csak uncomment

--- e2e/e2e_tests.spec.ts
+++ e2e/e2e_tests.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('should check the page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('https://staging.joladnijo.jmsz.hu/')
+  //await page.goto('/')
   // The home page should contain an h1 with "Jól adni jó"
-  await expect(page.locator('h1')).toContainText('Jól adni jó')
+  //await expect(page.locator('h1')).toContainText('Jól adni jó')
 })
