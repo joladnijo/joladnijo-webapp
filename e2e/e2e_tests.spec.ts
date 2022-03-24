ba3677c6e7636e4dbc6690a665edd3804014f625
Csaba Gajdos <info@cleancut.hu>
2022-03-24 06:51:26 +0100

    Teszt miatt kipróbálom, hogy a stagingen lefut-e

--- e2e/e2e_tests.spec.ts
+++ e2e/e2e_tests.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
-test('should navigate to the Documentation page', async ({ page }) => {
+test('should check the page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/')
+  await page.goto('https://staging.joladnijo.jmsz.hu/')
   // The home page should contain an h1 with "Jól adni jó"
   await expect(page.locator('h1')).toContainText('Jól adni jó')
 })
