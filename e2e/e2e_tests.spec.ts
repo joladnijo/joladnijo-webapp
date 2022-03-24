215db7294012ae2e7f50fcbce22aa2e21147c1cf
Gajdos Csaba <GCsabi@users.noreply.github.com>
2022-03-24 14:55:01 +0100

    Update e2e/e2e_tests.spec.ts
    Co-authored-by: Bence Molnár <molnarbence@users.noreply.github.com>

--- e2e/e2e_tests.spec.ts
+++ e2e/e2e_tests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('should check the page', async ({ page }) => {
+test('should check the home page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   //await page.goto('/')
   // The home page should contain an h1 with "Jól adni jó"
