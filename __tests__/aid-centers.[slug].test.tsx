95366e90763e369d496ef0b19777c752d2396324
Molnár Bence <xjarekx@gmail.com>
2022-03-27 00:39:34 +0100

    Pass the test again

--- __tests__/aid-centers.[slug].test.tsx
+++ __tests__/aid-centers.[slug].test.tsx
@@ -12,7 +12,7 @@ describe('AidCenter Info page', () => {
     render(<AidCenterInfoPage {...props} />);
 
     // assert
-    const requiredText = screen.getByText('Adományküldés előtt kérjük hívja fel a gyűjtőpont2ot!');
+    const requiredText = screen.getByText('Adományküldés előtt kérjük hívja fel a gyűjtőpontot!');
     expect(requiredText).toBeInTheDocument();
   });
 
@@ -26,9 +26,5 @@ describe('AidCenter Info page', () => {
     // assert
     const requiredText = screen.queryByText('Adományküldés előtt kérjük hívja fel a gyűjtőpontot!');
     expect(requiredText).toBeNull();
-  });
-
-  it('Foo', () => {
-    expect(2).toBe(1);
   });
 });
