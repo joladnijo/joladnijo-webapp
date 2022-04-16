21883e2a731852c9d71513e430cf60ca7f512170
Bence Molnár <molnarbence@users.noreply.github.com>
2022-04-16 23:48:49 +0200

    Fix backend urls (#41)

--- lib/apiHelpers.ts
+++ lib/apiHelpers.ts
@@ -1,8 +1,8 @@
 export const getBackendBaseUrl = () => {
   const { ENVIRONMENT } = process.env;
   if (ENVIRONMENT === 'staging') {
-    return 'https://api.staging.joladnijo.jmsz.hu';
+    return 'https://api.staging.joladnijo.hu';
   } else if (ENVIRONMENT === 'production') {
-    return 'https://api.staging.joladnijo.jmsz.hu';
+    return 'https://api.joladnijo.jmsz.hu';
   } else return 'http://localhost:8000';
 };
